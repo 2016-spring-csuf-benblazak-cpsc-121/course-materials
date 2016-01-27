d9eef78876732c0e038dd5406b885bb6a3eee29c
Ben Blazak <benblazak.dev@gmail.com>
2016-01-27 00:02:56 -0800

    adding lecture notes; merging correction

---
+++ test/students/grades.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3 -B
+# -----------------------------------------------------------------------------
+# Copyright &copy; 2016 Ben Blazak <bblazak@fullerton.edu>
+# Released under the [MIT License] (http://opensource.org/licenses/MIT)
+# -----------------------------------------------------------------------------
+
+# TODO: need a way to 
+# - override grades (especially to, e.g., W)
+# - add extra credit
+
