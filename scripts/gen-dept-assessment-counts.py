3191e003d696d0fcb1be2ce6d684c86b42c39f14
Ben Blazak <benblazak.dev@gmail.com>
2016-05-26 23:13:55 -0700

    now splitting assessment counts by section

--- scripts/gen-dept-assessment-counts.py
+++ scripts/gen-dept-assessment-counts.py
@@ -16,23 +16,35 @@ os.chdir(filedir)
 sys.path.insert(1, '..')
 
 import grades
+import students
 
 del sys.path[0]
 
 # -----------------------------------------------------------------------------
 
-values = [ v for k,v in grades.grades.items() if 'si' not in k ]
+sections = [ s['section'] if 'section' in s else None
+             for s in students.students.values() ]
+sections = [ s for s in sections if s is not None ]
+sections = set(sections)
 
-cmnt = [ g[('design','documentation')]['overall'] for g in values ]
-code = [ g[('review','functions')]['overall'] for g in values ]
-oop  = [ g[('object-oriented programming','classes')]['overall'] for g in values ]
+for section in sections:
+    values = [ v for k,v in grades.grades.items()
+               if 'si' not in k
+               and 'section' in students.students[k]
+               and students.students[k]['section'] == section ]
 
-for i in 'cmnt', 'code', 'oop':
-    print(i)
-    scores = globals()[i]
-    print( '    sat', len([ s for s in scores if s is not None and s >= 3.5 ]) )
-    print( '    dev', len([ s for s in scores if s is not None and s == 3 ]) )
-    print( '  unsat', len([ s for s in scores if s is None or s <= 2 ]) )
-    print( '  total', len(scores) )
-    print()
+    cmnt = [ g[('design','documentation')]['overall'] for g in values ]
+    code = [ g[('review','functions')]['overall'] for g in values ]
+    oop  = [ g[('object-oriented programming','classes')]['overall'] for g in values ]
+
+    print( '-------', 'cpsc 121-' + section, '-------' )
+
+    for i in 'cmnt', 'code', 'oop':
+        print(i)
+        scores = globals()[i]
+        print( '    sat', len([ s for s in scores if s is not None and s >= 3.5 ]) )
+        print( '    dev', len([ s for s in scores if s is not None and s == 3 ]) )
+        print( '  unsat', len([ s for s in scores if s is None or s <= 2 ]) )
+        print( '  total', len(scores) )
+        print()
 
