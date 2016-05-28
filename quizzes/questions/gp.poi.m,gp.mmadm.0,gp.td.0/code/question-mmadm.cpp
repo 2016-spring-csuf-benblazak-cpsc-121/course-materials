5980089147c37b34aa74c06e5e8497e08c4d8cd0
Ben Blazak <benblazak.dev@gmail.com>
2016-05-27 18:12:58 -0700

    moving files, to merge into course-materials

--- quizzes/questions/gp.poi.m,gp.mmadm.0,gp.td.0/code/question-mmadm.cpp
+++ quizzes/questions/gp.poi.m,gp.mmadm.0,gp.td.0/code/question-mmadm.cpp
@@ -5,10 +5,10 @@ int main() {
     const int SIZE = 7;
     // SECTION BEGIN all
     // C++ style
-    double * a = new double[SIZE];
+    char * a = new char[SIZE];
     delete[] a;
     // C style
-    double * b = (double *) malloc( sizeof(double) * SIZE );
+    char * b = (char *) malloc( sizeof(char) * SIZE );
     free(b);
     // SECTION END all
 
